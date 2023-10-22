ca1034550acf8f51d73570474f458b75d421e9da
ItzSt0rmz <thecolehawkins@gmail.com>
2023-10-22 12:56:11 -0500

    Setting up lemlib

---
+++ Emu2/src/main.cpp
@@ -0,0 +1,136 @@
+#include "main.h"
+using namespace pros;
+
+/**
+ * Runs initialization code. This occurs as soon as the program is started.
+ *
+ * All other competition modes are blocked by initialize; it is recommended
+ * to keep execution time for this mode under a few seconds.
+ */
+void initialize() {
+	chassis.calibrate();
+	lcd::initialize();
+	lcd::set_text(1, "Hello PROS User!");
+}
+
+/**
+ * Runs while the robot is in the disabled state of Field Management System or
+ * the VEX Competition Switch, following either autonomous or opcontrol. When
+ * the robot is enabled, this task will exit.
+ */
+void disabled() {}
+
+/**
+ * Runs after initialize(), and before autonomous when connected to the Field
+ * Management System or the VEX Competition Switch. This is intended for
+ * competition-specific initialization routines, such as an autonomous selector
+ * on the LCD.
+ *
+ * This task will exit when the robot is enabled and autonomous or opcontrol
+ * starts.
+ */
+void competition_initialize() {}
+
+/**
+ * Runs the user autonomous code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the autonomous
+ * mode. Alternatively, this function may be called in initialize or opcontrol
+ * for non-competition testing purposes.
+ *
+ * If the robot is disabled or communications is lost, the autonomous task
+ * will be stopped. Re-enabling the robot will restart the task, not re-start it
+ * from where it left off.
+ */
+void autonomous() {}
+
+/**
+ * Runs the operator control code. This function will be started in its own task
+ * with the default priority and stack size whenever the robot is enabled via
+ * the Field Management System or the VEX Competition Switch in the operator
+ * control mode.
+ *
+ * If no competition control is connected, this function will run immediately
+ * following initialize().
+ *
+ * If the robot is disabled or communications is lost, the
+ * operator control task will be stopped. Re-enabling the robot will restart the
+ * task, not resume it from where it left off.
+ */
+void opcontrol() {
+	FL.set_encoder_units(E_MOTOR_ENCODER_DEGREES);
+    FR.set_encoder_units(E_MOTOR_ENCODER_DEGREES);
+    BL.set_encoder_units(E_MOTOR_ENCODER_DEGREES);
+    BR.set_encoder_units(E_MOTOR_ENCODER_DEGREES);
+    TL.set_encoder_units(E_MOTOR_ENCODER_DEGREES);
+    TR.set_encoder_units(E_MOTOR_ENCODER_DEGREES);
+    CL.set_encoder_units(E_MOTOR_ENCODER_DEGREES);
+    CR.set_encoder_units(E_MOTOR_ENCODER_DEGREES);
+}
+
+/*************************/
+/*  NON VEX CODE 		 */
+/*************************/
+
+// Setup
+Motor FL(3, E_MOTOR_GEARSET_06, 1);
+Motor FR(6, E_MOTOR_GEARSET_06, 0);
+Motor BL(10, E_MOTOR_GEARSET_06, 1);
+Motor BR(5, E_MOTOR_GEARSET_06, 0);
+Motor TL(8, E_MOTOR_GEARSET_06, 0);
+Motor TR(4, E_MOTOR_GEARSET_06, 1);
+Motor CL(17, E_MOTOR_GEARSET_18, 1);
+Motor CR(18, E_MOTOR_GEARSET_18, 1);
+
+MotorGroup leftMotors({FL, BL, TL});
+MotorGroup rightMotors({FR, BR, TR});
+
+Controller Controller1(CONTROLLER_MASTER);
+Controller Controller1(CONTROLLER_PARTNER);
+
+Imu Inr(16);
+
+lemlib::Drivetrain_t drivetrain {
+	&leftMotors,
+	&rightMotors,
+	10, // distance between front wheels
+	3.25, // wheel diameter
+	360 // wheel rpm (gearing * motor rpm)
+};
+
+// forward/backward PID
+lemlib::ChassisController_t lateralController {
+    8, // kP
+    30, // kD
+    1, // smallErrorRange
+    100, // smallErrorTimeout
+    3, // largeErrorRange
+    500, // largeErrorTimeout
+    5 // slew rate
+};
+ 
+// turning PID
+lemlib::ChassisController_t angularController {
+    4, // kP
+    40, // kD
+    1, // smallErrorRange
+    100, // smallErrorTimeout
+    3, // largeErrorRange
+    500, // largeErrorTimeout
+    3 // slew rate
+};
+
+// odometry struct
+lemlib::OdomSensors_t sensors {
+	nullptr, //vertical tracking wheel 1
+	nullptr, //vertical tracking wheel 2
+	nullptr,
+    nullptr, // we don't have a second tracking wheel, so we set it to nullptr
+    &Inr // inertial sensor
+};
+
+// create the chassis
+lemlib::Chassis chassis(drivetrain, lateralController, angularController, sensors);
+
+
+
